94ff8193131d901851c2edc0577e44125a6aa902
Christopher Long <galizur@gmail.com>
2015-01-18 20:08:13 -0500

    Neglected case of electingto kick FG on 1st possession.

--- overtime/overtime.py
+++ overtime/overtime.py
@@ -17,21 +17,27 @@ offensive_td = 0.200
 
 offensive_score = field_goal + offensive_td
 
-# 1st possession no score
+# no offensive score
 
-no_score_1 = 1.0 - offensive_td - defensive_score
+no_of_score = 1.0 - offensive_score
 
-# n > 1 possession no score
+# no score by offense or defense
 
 no_score = 1.0 - offensive_score - defensive_score
 
 # Winning if you win toss:
 
 # touchdown on 1st possession
+# field goal on 1st possession, no of score on 2nd possession
+# field goal on 1st and 2nd possessions, win in sudden death
 # no score on 1st possession, defensive score on 2nd possession
-# no score on 1st or 2nd possessions, winn in sudden death state
+# no score on 1st or 2nd possessions, win in sudden death
 
-# win = offensive_td + no_score_1*defensive_score + no_score_1*no_score*sd_win
+# win = offensive_td +
+#       field_goal*no_of_score +
+#       field_goal*field_goal*sd_win +
+#       no_score*defensive_score +
+#       no_score*no_score*sd_win
 
 # Sudden death win probability:
 
@@ -47,6 +53,6 @@ sd_win = (offensive_score + no_score*defensive_score)/(1-no_score**2)
 
 print("Sudden death winning probility = {0:.3f}".format(sd_win))
 
-win = offensive_td + no_score_1*defensive_score + no_score_1*no_score*sd_win
+win = offensive_td + field_goal*no_of_score + field_goal*field_goal*sd_win + no_score*defensive_score + no_score*no_score*sd_win
 
 print("Overall winning probility = {0:.3f}".format(win))
