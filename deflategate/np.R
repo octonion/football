34d05270fb2f938c368a0c7e3f6383b060f46a68
Christopher D. Long <galizur@gmail.com>
2015-05-06 21:30:13 -0400

    Very simple non-parametric test assuming Colts were at 13.5 PSI.

--- deflategate/np.R
+++ deflategate/np.R
@@ -5,8 +5,8 @@ psi <- read.csv("psi.csv")
 
 patriots <- subset(psi, team=="Patriots")
 colts <- subset(psi, team=="Colts")
-colts$delta <- colts$delta - 0.5
+colts$delta <- colts$delta - 0.50
 
 psi <- rbind(patriots, colts)
 
-wilcox.test(delta ~ gauge, data=psi) 
+wilcox.test(delta ~ team, data=psi) 
