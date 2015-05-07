182ffa55256d5b248d375f5a420ea1cd04cf367c
Christopher D. Long <galizur@gmail.com>
2015-05-06 20:54:08 -0400

    Basic mixed model.

---
+++ deflategate/report.R
@@ -0,0 +1,17 @@
+
+library(lme4)
+
+psi <- read.csv("psi.csv")
+
+model0 <- delta ~ gauge + (1|ball)
+model <- delta ~ team + gauge + team:gauge + (1|ball)
+
+fit0 <- lmer(model0, data=psi)
+fit0
+summary(fit0)
+
+fit <- lmer(model, data=psi)
+fit
+summary(fit)
+
+anova(fit0,fit)
