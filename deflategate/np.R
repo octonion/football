c99e6e2673082b724599d4b38e60d6bde6634538
Christopher D. Long <galizur@gmail.com>
2015-05-11 22:49:22 -0400

    Added 13.35 PSI.

--- deflategate/np.R
+++ deflategate/np.R
@@ -16,6 +16,12 @@ colts$delta <- colts$delta - 0.25
 psi <- rbind(patriots, colts)
 wilcox.test(delta ~ team, data=psi)
 
+# Colts at 13.35 PSI
+
+colts$delta <- colts$delta - 0.10
+psi <- rbind(patriots, colts)
+wilcox.test(delta ~ team, data=psi)
+
 # Colts at 13.50 PSI
 
 colts$delta <- colts$delta - 0.25
