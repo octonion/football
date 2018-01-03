6d48171d44f90c54b6bc216d0c292fb821748143
Christopher D. Long <galizur@gmail.com>
2018-01-03 01:06:31 -0500

    Superbowl probabilities.

---
+++ odds/odds.R
@@ -0,0 +1,36 @@
+
+#library(lme4)
+
+sbn <- read.csv("sbn.csv")
+
+sbn$p <- sbn$den/(sbn$den+sbn$num)
+
+s <- sum(sbn$p)
+sbn$p <- log(sbn$p/s)
+
+model <- p ~ afc + nfc - 1
+
+
+fit <- lm(model, data=sbn)
+fit
+summary(fit)
+
+c <- data.frame(exp(coef(fit)))
+colnames(c) <- c("exp")
+
+af <- data.frame(1.0)
+rownames(af) <- c("nfcAtlanta Falcons")
+colnames(af) <- c("exp")
+
+c <- rbind(c,af)
+
+afc <- subset(c,startsWith(row.names(c), "afc"))
+nfc <- subset(c,startsWith(row.names(c), "nfc"))
+
+afc$p <- afc$exp/sum(afc$exp)
+nfc$p <- nfc$exp/sum(nfc$exp)
+
+afc
+nfc
+
+
